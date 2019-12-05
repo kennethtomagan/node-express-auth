b657ae3599b8dcad7c96fca73e682e3dc1fcd27f
kenneth <kenneth.tomagan123@gmail.com>
2019-12-05 16:53:48 +0800

    register done

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation} = require('../validation');
+
 
 router.post('/register', async (req, res) => {
-
+    // Validate Data before create user
+    const {error} = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message);
+    // Checking if the user is already in database
 
     const user = new User({
         name: req.body.name,
@@ -18,6 +23,7 @@ router.post('/register', async (req, res) => {
         res.status(400).send(err);
     }
 })
+
 
 // router.post('/login')
 
---
+++ validation.js
@@ -0,0 +1,32 @@
+
+// VALIDATION
+const Joi = require('@hapi/joi');
+
+// Register Validation
+const registerValidation = data =>{
+
+    const schema = {
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    }
+
+   return Joi.validate(data, schema);
+
+};
+
+
+// Login Validation
+const loginValidation = data =>{
+
+    const schema = {
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    }
+
+   return Joi.validate(data, schema);
+
+};
+
+module.exports.registerValidation = registerValidation
+module.exports.registerValidation = loginValidation
